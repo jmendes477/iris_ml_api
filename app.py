464c5030d8fe6b7841d544abe82c9e6fc6fdff4e
jmendes <zemendes725@gmail.com>
2024-05-14 23:10:03 +0100

    simple iris ml model

---
+++ app.py
@@ -0,0 +1,29 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+model = pickle.load(open('iris_classifier_model.pkl', 'rb'))
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+      return render_template('index.html')
+
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [float(x) for x in request.form.values()]
+   
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output =prediction[0]
+
+    return render_template('index.html', prediction_text='The Flower is {}'.format(output))
+
+if __name__ == "__main__":
+    app.run(debug=True)
